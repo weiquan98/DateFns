d7f9fa614cc4b9a5b22a296683b272c5d9ba068d
Quan <30767205+weiquan98@users.noreply.github.com>
2025-10-01 19:31:53 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/datefns/main.go
@@ -0,0 +1,23 @@
+// cmd/datefns/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "datefns/internal/datefns"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := datefns.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
